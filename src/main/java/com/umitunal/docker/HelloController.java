328f7723caaa8267b1e77b0ef75ffe50b2a1f08d
Ümit Ünal <umit.unal.89@gmail.com>
2016-03-01 09:52:19 +0200

    first commit

---
+++ src/main/java/com/umitunal/docker/HelloController.java
@@ -0,0 +1,16 @@
+package com.umitunal.docker;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping("/{name}")
+    public String hello(@PathVariable String name) {
+        return "Hello " + name;
+    }
+
+}
